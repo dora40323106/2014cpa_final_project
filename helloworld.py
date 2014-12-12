ecc3a51b61bf5210704cedec7e5390dfbd188641
chiaming yen <chiamingyen@gmail.com>
2014-12-12 08:39:42 +0800

    add coding, use int(), add for loop, use range()

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,8 @@
-toprint = input("What do you want to print?")
-print(toprint)
+# coding=utf-8
+# 上面的程式內容編碼必須在程式的第一或者第二行才會有作用
+# 利用 input() 取得的資料型別為字串
+toprint = input("要印甚麼內容?")
+# 若要將 input() 取得的字串轉為整數使用, 必須利用 int() 轉換
+repeat_no = int(input("重複列印幾次?"))
+for i in range(repeat_no):
+    print(toprint)
