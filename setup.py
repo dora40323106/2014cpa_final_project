86fd43e57aa71398c8135ff8c780c70a8a2f99f5
40323105 <40323105@gm.nfu.edu.tw>
2014-12-15 20:02:28 +0800

    x

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+#@+leo-ver=5-thin
+#@+node:2015.20141215191725.1773: * @file V:\cpw15\setup.py
+#@@language python
+#@@tabwidth -4
+#@+others
+#@+node:2015.20141215191725.1775: ** setup declarations
 from setuptools import setup
 
 setup(name='YourAppName',
@@ -6,5 +12,7 @@ setup(name='YourAppName',
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Cherrypy'],
      )
+#@-others
+#@-leo
