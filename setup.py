9d4f52e1f9dc7614959e0c642ec42d2e2034781f
40323105 <40323105@gm.nfu.edu.tw>
2014-12-22 19:49:02 +0800

    change a

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
-#@+leo-ver=5-thin
-#@+node:2015.20141215191725.1773: * @file V:\cpw15\setup.py
-#@@language python
-#@@tabwidth -4
-#@+others
-#@+node:2015.20141215191725.1775: ** setup declarations
 from setuptools import setup
 
 setup(name='YourAppName',
@@ -12,7 +6,5 @@ setup(name='YourAppName',
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Cherrypy'],
-     )
-#@-others
-#@-leo
+      install_requires=['Cherrypy', 'mako'],
+      )
