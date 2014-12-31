dede5c75479d63edccce519670316eddfa037ffe
chiaming yen <chiamingyen@gmail.com>
2014-12-31 10:11:07 +0800

    Luke

--- std/example2.py
+++ std/example2.py
@@ -13,24 +13,24 @@ class Application(object):
     #@+node:lee.20141223114246.42: *3* def init
     def __init__(self):
     	#你的名子
-        self.name = '林祐生'
+        self.name = '呂致安'
         # 你的學號
-        self.number = '40323123'
+        self.number = '40323116'
         # 你的班級
         self.classes = 'nfu'
         # 你的 github repository url
-        self.github_repo_url = 'https://github.com/dora40323106/2014cpa_final_project.git'
+        self.github_repo_url = 'https://github.com/dora40323106/2014cpa_final_project'
         # 你的 bitbucket repository url
         self.bitbucket_repo_url = ''
         # 你的 openshift app
         self.openshift_url = 'http://cheerpy-40323123.rhcloud.com/'
         # 你的自評
-        self.evaluation = [('Project 7', 80), ('Project 8', 90), ('Project 9', 100)]
+        self.evaluation = [('Project 7', 70), ('Project 8', 70), ('Project 9', 70)]
         # 你的照片 url
-        self.photo_url = 'http://placekitten.com/g/350/300'
+        self.photo_url = 'https://copy.com/FyApBXapg7hbCITi'
         # 這裡是心得
         self.my_remark = """
-        Computer Programming is good course
+        整個好複雜阿  要把全部的東西串再一起，觀念要很清楚 不然整個完蛋。
         """
 
     #@+node:lee.20141223114246.43: *3* def use_template
