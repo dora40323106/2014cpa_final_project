6b5ffa23a5cb12a0b10748e175e1734f4a9526fa
chiaming yen <chiamingyen@gmail.com>
2015-01-05 20:02:14 +0800

    DD

--- std/a40323137.py
+++ std/a40323137.py
@@ -25,13 +25,13 @@ class Application(object):
         # 你的自評
         self.evaluation = [('Project 7', 75), ('Project 8', 75), ('Project 9', 75)]
         # 你的照片 url
-        self.photo_url = 'https://copy.com/2VtzHVIN0gJDr1FD'
+        self.photo_url = 'https://copy.com/Tsn966BKSO8tdBMB'
         # 這裡是心得
         self.my_remark = """
         在課程前端，雖然摸不著頭緒，但有老師使用CamStudio記錄著影片，
         讓後學能夠自己打造環境；在錯誤中透過老師的教學影片，能夠察覺
         自己的錯誤所在，加上在班上有許多熱心的同學幫忙，這些人打前鋒，讓我們這些迷途羔羊，如
-        在黑暗中受到燈塔指引，一切皆明朗。只是有沒有心要多練習而已。
+        在黑暗中受到燈塔指引，一切皆明朗。只是看有沒有心要多練習而已。
         """
 
     #@+node:lee.20141223114246.43: *3* def use_template
@@ -220,11 +220,7 @@ class Application(object):
         return self.use_template(content)
     #@+node:lee.20141223114246.47: *3* def link
     def link(self):
-<<<<<<< HEAD
-        aviable_link = [("index", "首頁"), ("remark", "心得"), (self.openshift_url, "個人 openshift app"),(self.github_repo_url, "個人 github repo"), ('/', 'back to list')]
-=======
         aviable_link = [("index", "首頁"), ("remark", "心得"), (self.openshift_url, "個人 openshift app"),(self.github_repo_url, "個人 github repo"),('/', 'back to list')]
->>>>>>> 86ab54258b2d5ffb69d7e77a50d4713fd65cfee8
         return aviable_link
     #@+node:lee.20141223114246.54: *3* def remark
     @cherrypy.expose
