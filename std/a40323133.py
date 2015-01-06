08944d013bad1956324f0e50bcc283b4ae254cc4
unknown <40323133@gm.nfu.edu.tw name = chiaming yen>
2015-01-06 20:04:30 +0800

    3133

--- std/a40323133.py
+++ std/a40323133.py
@@ -1,215 +1,245 @@
 #@+leo-ver=5-thin
-#@+node:lee.20141215164031.94: * @file example.py
+#@+node:lee.20141224110313.61: * @file example2.py
 #@@language python
 #@@tabwidth -4
-#@+<<decorations>>
-#@+node:lee.20141215164031.95: ** <<decorations>>
 import cherrypy
 import random
 from std.asciisymbol import asciiImage
-from wsgi import env
-#@-<<decorations>>
 
 #@+others
-#@+node:lee.20141215164031.96: ** class Application
+#@+node:lee.20141223114246.41: ** class Application
 class Application(object):
     #@+others
-    #@+node:lee.20141221203113.62: *34* def __init__
+    #@+node:lee.20141223114246.42: *3* def init
     def __init__(self):
+    	#你的名子
         self.name = '張庭豪'
+        # 你的學號
         self.number = '40323133'
-
-        self.classes = '四機械設計一甲'
-        self.github_repo_url = 'https://github.com/mdeta/2014cp'
-
-        self.evaluation = [('Project1 ', 80), ('Project2 ', 80), ('Project3 ', 80)]
+        # 你的班級
+        self.classes = 'nfu'
+        # 你的 github repository url
+        self.github_repo_url = 'https://github.com/dora40323106/2014cpa_final_project'
+        # 你的 bitbucket repository url
+        self.bitbucket_repo_url = ''
+        # 你的 openshift app
+        self.openshift_url = ''
+        # 你的自評
+        self.evaluation = [('Project 7', 80), ('Project 8', 80), ('Project 9', 80)]
+        # 你的照片 url
         self.photo_url = 'https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/10354900_577381035706449_745481432372504952_n.jpg?oh=840e0779ae920a5e8e21f0b1c71a3bf0&oe=54F3AEDE&__gda__=1424476131_874c128407d3267ccfce0b364b9b4b56'
-    def get_nav(self):
+        # 這裡是心得
+        self.my_remark = """
+        我覺得計算機程式這門課對我來說不太好學，但經過這好幾週的課後我還是學會了一些東西，像使用OpenShift和GitHub做版次管理，
+        可以用幾個簡單的指令和同學合作寫程式，雖然最後的期末報告我也只會修改範例，但是能自己弄完也是有一些成就感。
         """
-        取得 nav link
+
+    #@+node:lee.20141223114246.43: *3* def use_template
+    def use_template(self, content):
+        above = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        
+      <!-- Basic Page Needs
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+      <meta charset="utf-8">
+      <title>title</title>
+      <meta name="description" content="">
+      <meta name="author" content="">
+
+      <!-- Mobile Specific Metas
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <!-- FONT
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <style>
+    @font-face {
+      font-family: 'Raleway';
+      font-style: normal;
+      font-weight: 300;
+      src: local('Raleway Light'), local('Raleway-Light'), url(/static/font/Raleway300.woff) format('woff');
+    }
+    @font-face {
+      font-family: 'Raleway';
+      font-style: normal;
+      font-weight: 400;
+      src: local('Raleway'), url(/static/font/Raleway400.woff) format('woff');
+    }
+    @font-face {
+      font-family: 'Raleway';
+      font-style: normal;
+      font-weight: 600;
+      src: local('Raleway SemiBold'), local('Raleway-SemiBold'), url(/static/font/Raleway600.woff) format('woff');
+    }
+    </style>
+
+      <!-- CSS
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+      <link rel="stylesheet" href="/static/css/normalize.css">
+      <link rel="stylesheet" href="/static/css/skeleton.css">
+      <link rel="stylesheet" href="/static/css/custom.css">
+      <!-- Favicon
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+      <link rel="icon" type="image/png" href="/static/images/favicon.png" />
+
+    </head>
+    <body>
+
+      <!-- Primary Page Layout
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <!-- .container is main centered wrapper -->
+    <div class="container">
+    """
+        below = """
+    </div>
+    <footer class="center">
+      2014 Computer Programming
+    </footer>
+
+    <!-- Note: columns can be nested, but it's not recommended since Skeleton's grid has %-based gutters, meaning a nested grid results in variable with gutters (which can end up being *really* small on certain browser/device sizes) -->
+
+    <!-- End Document
+      –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    </body>
+    </html>
+    """
+        return above + self.generate_nav(self.link()) + content + below
+    #@+node:lee.20141223114246.44: *3* def generate_nav
+    def generate_nav(self, anchors):
+        above_side = """
+        <div class="row">
+            <div class="nav twelve columns">
+                <input type="checkbox" id="toggle" />
+                <div>
+                    <label for="toggle" class="toggle" data-open="Main Menu" data-close="Close Menu" onclick></label>
+                    <ul class="menu">
         """
-        #(URL 路徑, anchor name)
-        anchors = [('index', 'home'), ('guessForm', '猜數字'), ('multipliedTable', '乘法表'), ('asciiForm', '使用圖案印出字'), (self.github_repo_url, 'github repository'), ('/', 'back to list')]
-        return anchors
-    #@+node:lee.20141215164031.98: *34* def index
+
+        content = ''
+        for link, name in anchors:
+            content += '<li><a href="' + link + '">' + name + '</a></li>'
+
+        below_side = """
+                    </ul>
+                </div>
+            </div>
+        </div>
+        """
+        return above_side + content + below_side
+    #@+node:lee.20141223114246.45: *3* def generate_form_page
+    def generate_form_page(self, form='', output=''):
+        content = """
+            <div class="content">
+            <div class="row">
+              <div class="one-half column">
+                %s
+              </div>
+              <div class="one-half column">
+                <div class="output u-full-width">
+                  <p>Output:</p>
+                  <p>
+                    %s
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        """%(form, output)
+        return self.use_template(content)
+    #@+node:lee.20141223114246.55: *3* def generate_headline_page
+    def generate_headline_page(self, headline, output):
+        content = """
+      <div class="content">
+        <div class="row">
+          <div class="headline center">%s</div>
+          <div class="twelve columns">
+            <p>%s</p>
+          </div>
+        </div>
+      </div>
+        """ % (headline, output)
+        return self.use_template(content)
+    #@+node:lee.20141223114246.46: *3* def generate_personal_page
+    def generate_personal_page(self, data=None):
+        if data is None:
+            return ''
+
+        # check data have all we need, if the key not exist, use empty string
+        must_have_key = ('photo_url', 'name', 'ID', 'class', 'evaluation')
+        for key in must_have_key:
+            data[key] = data.get(key, '')
+
+
+        if 'evaluation' in data:
+            table_content = ''
+            for projectName, score in data['evaluation']:
+                table_content += """<tr><td>%s</td><td>%s</td>"""%(projectName, score)
+            data['evaluation'] = table_content
+        content = """
+    <div class="content">
+    <div class="row">
+      <div class="one-half column">
+        <div class="headline">
+          About Me
+        </div>
+        <div class="photo">
+          <img src="{photo_url:s}" alt="photo">
+        </div>
+        <div class="meta">
+          <ul>
+            <li>Name: {name:s}</li>
+            <li>ID NO. : {ID:s}</li>
+            <li>Class: {class:s}</li>
+          </ul>
+        </div>
+      </div>
+      <div class="one-half column">
+        <div class="headline">
+          Self Evaluation
+        </div>
+        <div>
+          <table class="u-full-width">
+            <thead>
+              <tr>
+                <th>Project Name</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+                {evaluation:s}
+            </tbody>
+          </table>
+
+        </div>
+      </div>
+    </div>
+    </div>
+        """.format(**data)
+        return self.use_template(content)
+    #@+node:lee.20141223114246.47: *3* def link
+    def link(self):
+        aviable_link = [("index", "HOME"), ("remark", "心得"), (self.openshift_url, "個人Openshift APP"),(self.github_repo_url, "個人github REPO"), (self.bitbucket_repo_url, "個人bitbucket REPO"), ('/', 'back to list')]
+        return aviable_link
+    #@+node:lee.20141223114246.54: *3* def remark
+    @cherrypy.expose
+    def remark(self):
+        # 這裡是心得
+        # generate_headline_page(你的標題, 你的內容)
+        return self.generate_headline_page("REMARK", self.my_remark)
+    #@+node:lee.20141223114246.48: *3* def index
     @cherrypy.expose
     def index(self):
-        """
-        個人首頁
-        """
-        # 取得模板
-        tmpl = env.get_template('personal_page.html')
-        # 設定額外變數
-        extra_content = {
-            'title': 'personal page -' + self.name,
-            'photo_url': self.photo_url,
-            'std_name': self.name,
+        # 這裡是首頁
+        data = {
+            'name':self.name,
             'ID':self.number,
-            # class 在 mako 底下是關鍵字
-            'classes':self.classes,
-            'anchors':self.get_nav(),
-            'self_evaluations':self.evaluation
+            'class':self.classes,
+            'evaluation': self.evaluation,
+            'photo_url':self.photo_url,
         }
-        # 宣染
-        return tmpl.render(**extra_content)
-
-    #@+node:lee.20141215164031.99: *3* def guessForm
-    @cherrypy.expose
-    def guessForm(self, guessNumber=None):
-        
-        # get template
-        tmpl = env.get_template('form.html')
-        
-        form = """
-        <form action="" method="get">
-          <label for="guessNumber">Guess Number(1~99)</label>
-          <input name="guessNumber" type="text">
-          <input type="submit" value="Send" class="button button-primary">
-        </form>
-        """
-        
-        # set common content
-        extra_content = {'title':'guessform', 'form':form, 'anchors':self.get_nav()}
-        
-        # get count from session
-        count = cherrypy.session.get("count", None)
-        # if is None, it mean it does not exist
-        if not count:
-            # create one
-            count = cherrypy.session["count"] = 0
-
-        # get answer from session
-        answer = cherrypy.session.get("answer", None)
-        # if is None, it mean it does not exist
-        if not answer:
-            # create one
-            answer = cherrypy.session["answer"] = random.randint(1, 100)
-
-
-        # 設定在哪種情況下該回傳哪種訊息
-        message = {
-            "welcome": "guess a number from 1 to 99",
-            "error": "must input a number, your input is %s" % str(guessNumber),
-            "successful": "correct! your input is %s answer is %d total count %d" % (str(guessNumber), answer, count),
-            "smaller": "smaller than %s and total count %d" % (str(guessNumber), count),
-            "bigger": "bigger than %s and total count %d" % (str(guessNumber), count),
-        }
-
-        #假如 guessNumber is None, 表示是第一次進來, 或是未傳值
-        if guessNumber is None:
-            extra_content['output'] = message['welcome']
-            return tmpl.render(**extra_content)
-        # 其他, 表示要開始處理 guessNumber
-        else:
-            # convert guessNumber to int
-            try:
-                guessNumber = int(guessNumber)
-            except:
-                # if fail
-                # throw error
-                extra_content['output'] = message['error']
-                return tmpl.render(**extra_content)
-
-            # convert ok, make count plus one, everytime
-            cherrypy.session["count"] += 1
-
-            if guessNumber == answer:
-                # clear session count and answer
-                del cherrypy.session["count"]
-                del cherrypy.session["answer"]
-                # throw successful
-                extra_content['form'] = ''
-                extra_content['output'] = message["successful"]+'<a href="guessForm">play again</a>'
-            elif guessNumber > answer:
-                # throw small than guessNumber
-                extra_content['output'] = message["smaller"]
-            else:
-                # throw bigger than guessNumber
-                extra_content['output'] = message["bigger"]
-            return tmpl.render(**extra_content)
-    #@+node:lee.20141215164031.100: *3* def multipliedTable
-    @cherrypy.expose
-    def multipliedTable(self, first=None, second=None):
-        # get template
-        tmpl = env.get_template('form.html')
-        # set up messages
-        message = {
-            'error': 'you must input correct type data.',
-            'welcome': 'Welcome to multiplied table page.',
-        }
-        # set up form
-        # two variable
-        # first, second
-        form = """
-        <form action="" method="post">
-          <label for="first">first number(an integer)</label>
-          <input name="first" type="text">
-          <label for="first">second number(an integer)</label>
-          <input name="second" type="text">
-          <input type="submit" value="Send" class="button button-primary">
-        </form>
-        """
-
-        # set extra content variable
-        extra_content = {
-            'title': 'multipliedTable', 'form': form, 'anchors': self.get_nav()}
-
-        # if first and second parameter is None
-        # set output to welcome
-        if first is None and second is None:
-            extra_content['output'] = message.get('welcome')
-            return tmpl.render(**extra_content)
-        # try convert to integer
-        try:
-            first = int(first)
-            second = int(second)
-        except:
-            #raise error
-            extra_content['output'] = message.get('error')
-            return tmpl.render(extra_content)
-        # start process
-        output = ''
-        for f in range(1, first + 1):
-            for s in range(1, second + 1):
-                output += str(f) + '*' + str(s) + '=' + str(f * s) + '<br/>'
-        # update extra content
-        extra_content['output'] = '<p>' + output + '</p>'
-        # render
-        return tmpl.render(**extra_content)
-    #@+node:lee.20141215164031.101: *3* def asciiForm
-    @cherrypy.expose
-    def asciiForm(self, text=None):
-        # get template
-        tmpl = env.get_template('form.html')
-        # set up messages
-        messages = {
-            'welcome': 'welcome to ascii form',
-        }
-        # set up form
-        # variable text
-        form = """
-        <form method="get" action="">
-            <label for="text">Say....</label>
-            <input type="text" name="text" />
-            <input type="submit" value="Send" class="button button-primary">
-        </form>
-        """
-        # set up extra content
-        extra_content = {
-            'title': 'asciiForm', 'form': form,
-            'anchors': self.get_nav()
-        }
-
-        # if text is None, set output to welcome
-        if text is None:
-            extra_content['output'] = messages.get('welcome')
-        # if not None, start process
-        else:
-            # use module asciisymol's asciiImage function
-            # one arg, input type string
-            extra_content['output'] = asciiImage(text)
-        # render
-        return tmpl.render(**extra_content)
+        return self.generate_personal_page(data)
     #@-others
 #@-others
 #@-leo
